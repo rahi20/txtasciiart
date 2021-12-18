31e3d1d93fc9fd297b47a97ea2f6033af82803d0
Rahima Largo <largorahima@gmail.com>
2021-12-18 17:56:13 +0100

    added rows

--- txtasciiart.py
+++ txtasciiart.py
@@ -4,36 +4,43 @@ def main():
     l = 4
     h = 5
     t = sys.argv[1:]
+
+    rows = [
+        " #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ### ",
+        "# # # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   # ",
+        "### ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   ## ",
+        "# # # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #       ",
+        "# # ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  #  "
+    ]
+
     if len(t) > 0 :
         t = " ".join(t)
 
-        with open("ascii", "r") as ascii_file :
-            rows = ascii_file.readlines()
-            init = ord("A")
-            ascii = []
-            for i in range(h):
-                row = rows[i]
-                line = ""
-                for c in list(t) :
+        init = ord("A")
+        ascii = []
+        for i in range(h):
+            row = rows[i]
+            line = ""
+            for c in list(t) :
              
-                    diff = ord(c.upper()) - init
+                diff = ord(c.upper()) - init
             
-                    if c == " " : 
-                        line += "    "
-                        continue 
+                if c == " " : 
+                    line += "    "
+                    continue 
 
-                    if diff > (ord("Z") - init) or diff < 0 :
-                        line += row[-1-(l-1):] 
-                        continue
+                if diff > (ord("Z") - init) or diff < 0 :
+                    line += row[-1-(l-1):] 
+                    continue
 
-                    line += row[diff * l : diff * l + l]
+                line += row[diff * l : diff * l + l]
 
-                ascii.append(line)
+            ascii.append(line)
 
-            for line in ascii:
-                print(line)
+        for line in ascii:
+            print(line)
     else : 
-        sys.exit("No arguments passed!")
+        sys.exit("No arguments passed")
 
     
 
